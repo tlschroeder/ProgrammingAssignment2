21fbbc872d37d5492aae4ddef6bbb9553620c9a3
tlschroeder <tlschroeder@email.wm.edu>
2016-03-20 17:26:57 -0400

    Solution
    Contains code to solve the designated assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
